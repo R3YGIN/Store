cf66e73d31afa5043aca88dac94331c62d0b40e8
R3YGIN <george.gerus@gmail.com>
2022-02-02 16:03:19 +0300

    start

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+	return <div className='container'>
+		<Navbar/>
+	</div>;
+};
+
+export default Home;
